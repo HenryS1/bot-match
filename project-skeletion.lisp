22b8fc7286d0f00e1d3c462286ce0ee143874015
Henry Steere <henry.steere@gmail.com>
2021-03-20 18:37:02 +0200

    Start on project skeleton script

---
+++ project-skeletion.lisp
@@ -0,0 +1,11 @@
+;; first install quicklisp
+(eval-when (:load-toplevel :compile-toplevel)
+  (ql:quickload :cl-project))
+
+(cl-project:make-project #p"runtime"
+                         :author "Henry and Ed"
+                         :email "henry.steere@gmail.com, edward.steere@gmail.com"
+                         :license "MIT"
+                         :depends-on (:cl-arrows :anaphora))
+
+
