7ea921c43a341e3886fc8eca424ece68c0682686
Edward Steere <edward.steere@gmail.com>
2022-02-12 18:03:09 +0200

    Add an example bot for Rust

---
+++ example-bots/rust/src/main.rs
@@ -0,0 +1,75 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Deserializer;
+use serde;
+use std::io;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Player {
+    money: i32,
+    base: Coord,
+    health: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MapEntry {
+    r#type: Box<str>,
+    position: Coord,
+    team: Option<Box<str>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all="kebab-case")]
+struct State {
+    you: Box<str>,
+    map: Vec<MapEntry>,
+    turns_remaining: i32,
+    player1: Player,
+    player2: Player,
+}
+
+fn compute_move(state: State) -> std::string::String {
+    let my_player = if "player1" == &*state.you {
+        &state.player1
+    } else {
+        &state.player2
+    };
+    let other_player = if "player1" == &*state.you {
+        &state.player2
+    } else {
+        &state.player1
+    };
+    if my_player.money >= 10 {
+        let my_base_coord = &my_player.base;
+        let enemy_base_coord = &other_player.base;
+        format!(
+            "BUILD SCOUT ({}, {}) ({}, {})",
+            my_base_coord.x,
+            my_base_coord.y + 1,
+            enemy_base_coord.x,
+            enemy_base_coord.y,
+        )
+    } else {
+        "NO-OP".to_string()
+    }
+}
+
+fn main() {
+    println!("READY");
+    let stdin = io::stdin();
+    let all_states = Deserializer::from_reader(stdin.lock()).into_iter::<State>();
+    for result in all_states {
+        match result {
+            Ok(state) => println!("{}", compute_move(state)),
+            Err(error) => {
+                eprintln!("{}", error);
+                println!("NO-OP");
+            }
+        }
+    }
+}
