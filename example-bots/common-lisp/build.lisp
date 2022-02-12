79e74243b27cfaa867bb908b5b4c07363a5274d5
Edward Steere <edward.steere@gmail.com>
2022-02-12 18:02:06 +0200

    Fix image and bot executable names in the example lisp bot

--- example-bots/common-lisp/build.lisp
+++ example-bots/common-lisp/build.lisp
@@ -1,7 +1,7 @@
 (push (merge-pathnames "") ql:*local-project-directories*)
 (asdf:load-system :bot-match-bot)
 (ql:quickload "bot-match-bot")
-(sb-ext:save-lisp-and-die #p"quiescent"
+(sb-ext:save-lisp-and-die #p"bot"
                           :compression 9
                           :save-runtime-options t
                           :toplevel #'bot-match-bot:main
