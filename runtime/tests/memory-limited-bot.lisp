4e25590a5385240f68e0167d4a32a156f19c8ee5
Henry Steere <henry.steere@gmail.com>
2022-03-16 21:03:11 +0200

    Allocate array to trigger OOM

--- runtime/tests/memory-limited-bot.lisp
+++ runtime/tests/memory-limited-bot.lisp
@@ -4,13 +4,14 @@
 (in-package :input-bot)
 
 (defun run ()
-  (let ((result (with-output-to-string (s)
-                  (loop for line = (read-line t nil nil)              
-                     do (write-line line s)
-                     while (listen)))))
-    (format t "~a" result)
-    (finish-output)
-    (sleep 2)))
+  (let ((arr (make-array 20000000 :initial-element 0)))
+    (let ((result (with-output-to-string (s)
+                    (loop for line = (read-line t nil nil)              
+                       do (write-line line s)
+                       while (listen)))))
+      (format t "~a" result)
+      (finish-output)
+      (sleep 2))))
 
 (defun main (&rest argv)
   "Start the bot."
